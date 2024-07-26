f415c3f53aec2f6acf4068a05b574150b9d3b1dd
SWARNA SAI SREEKAR <101892496+sreekar0122@users.noreply.github.com>
2024-07-26 19:15:42 +0530

    Update App.js

--- FakeStoreAPI/App.js
+++ FakeStoreAPI/App.js
@@ -1,21 +1,57 @@
+import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
-import GetAllProducts from './component/GetAllProducts/GetAllProducts.js';
+import Menu from "./Menu";
+import Login from './Login';
+import Footer from './Footer';
+import Container from './Container';
+import Counter from './Counter';
+import Todo from './Todo';
+import Hobbies from './Assignment';
+import Add from './2nd';
+import Trigonometry from './3rd';
+import ObjectList from './4th';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import GenderAPI from './GenderAPI';
+import Products from './Products';
+import MyProfile from './Profile';
+import Github from './github';
+import Fake from './Fake';
+import ProductDetailsComponent from './ProductDetailsComponent';
+
 function App() {
-  return (
-    <div className="App clearfix">
-      <BrowserRouter>
-      <div className="button-container">
-        <Link to="/GetAllProducts" className="button products">GetAllProducts</Link>
-      </div>
-      <div className='button-container'>
-      <Routes>
-        <Route path='/GetAllProducts' element={<GetAllProducts />}/>
-      </Routes>
-      </div>
+
+    const LOGIN_URL="https://ascendion.com/login";
+    let login_attempts=5;
+    let menuData=[
+      {title:"Home",path:"/"},
+      {title:"Todos",path:"/Todo"},
+      {title:"Login",path:"/login/:title/:tokenId"}
+    ];
+    function greet(){
+      alert("Let's Login! You have a great day!");
+    }
+    return(
+      <div className="App">
+
+        <br/>
+
+        
+        <BrowserRouter>
+          <Link to="/Home"> Home </Link>   &nbsp;  
+          <Link to="/All_Categories"> All Products </Link>  &nbsp;<br/>
+          <br/><br/><br/>
+          <Routes>
+            <Route path="/:category" element={<Fake />} />
+            <Route path="/:category" element={<Products />} />
+            <Route path='/:category/:id' element={<ProductDetailsComponent/>}></Route>
+          </Routes>
       </BrowserRouter>
-    </div>
-  );
+
+      <footer>
+         <Footer/>
+       </footer> 
+      </div>
+    );
 }
 
 export default App;
